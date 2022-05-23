4ddc62d4bb89aa6c9db2897119d89473be98278a
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-05-23 10:04:20 -0700

    commit tree b7903c07bc9ed0b1f7982a19bffaad4378efef8a

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData (name, email) {
+    const userData = {
+        name: name,
+        email: email
+    };
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+
+    return fetch ("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(object => {
+        const userID = object['id'];
+        const p = document.createElement('p');
+        p.textContent = userID;
+
+        document.body.appendChild(p);
+    })
+    .catch(error => {
+        const p = document.createElement('p');
+        p.textContent = error.message;
+        document.querySelector('body').appendChild(p);
+    });
+}
+
+
+
+
